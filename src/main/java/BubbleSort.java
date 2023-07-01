cc26fa50944f1291d104bae68ea52001f3da87c1
michal-jamula <michal5832@gmail.com>
2023-07-01 19:09:41 +0100

    updated comments

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,17 +1,17 @@
 package main.java;
 
-//Collection of sorting algorithms, more will be added in the future
+
+/**
+ Bubble sort
+ Best and Worst time complexity: O(n) - O(n^2)
+ Space complexity: O(1)
+ */
 public class BubbleSort {
 
-
-    /**
-        Bubble sort
-        Best and Worst time complexity: O(n) - O(n^2)
-        Space complexity: O(1)
-     */
     public static void sort (int[] numbers) {
-
+        //first loop
         for (int first = 1; first < numbers.length; first++) {
+            //inner loop
             for (int second = 0; second < first; second++) {
 
                 //Swap numbers if needed
